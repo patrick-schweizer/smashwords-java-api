48825e31b237ee4956983d6ae3f93739156f02b6
patrick.schweizer.ing@gmail.com <patrick.schweizer.ing@gmail.com@9488c6e7-2cef-d8bb-f510-c2d52d8d2cfe>
2012-05-11 18:26:17 +0000

    initial commit

---
+++ SmashwordsAPI/src/test/java/com/sw/access/modify/PageLoaderTest.java
@@ -0,0 +1,31 @@
+package com.sw.access.modify;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Test;
+
+import com.unleashyouradventure.swapi.load.PageLoader;
+
+public class PageLoaderTest {
+    private File downloadFile;
+
+    @Test
+    public void testGetFile() throws IOException {
+        // TODO
+        downloadFile = new PageLoader().saveURLToFile(new File(System.getProperty("java.io.tmpdir")),
+                "http://www.smashwords.com/books/download/131185/8/latest/0/1/happily-ever-after_10pct_sample.epub");
+        assertNotNull(downloadFile);
+        assertTrue(downloadFile.length() > 0);
+    }
+
+    @After
+    public void tearDown() {
+        downloadFile.deleteOnExit();
+        downloadFile.delete();
+    }
+}
