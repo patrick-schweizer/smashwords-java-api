3d6dd917d0407758c4603301179b588be8a7c9b9
patrick.schweizer.ing@gmail.com <patrick.schweizer.ing@gmail.com@9488c6e7-2cef-d8bb-f510-c2d52d8d2cfe>
2012-05-12 19:41:55 +0000

    Refactoring

--- trunk/SmashwordsAPI/src/main/java/com/unleashyouradventure/swapi/cache/InMemoryCache.java
+++ trunk/SmashwordsAPI/src/main/java/com/unleashyouradventure/swapi/cache/InMemoryCache.java
@@ -4,18 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.unleashyouradventure.swapi.retriever.Book;
-import com.unleashyouradventure.swapi.retriever.LazyArrayList;
+import com.unleashyouradventure.swapi.retriever.BookList;
 
 public class InMemoryCache implements Cache {
 
     private Map<Long, Book> books = new HashMap<Long, Book>();
-    private Map<String, LazyArrayList<Book>> lists = new HashMap<String, LazyArrayList<Book>>();
+    private Map<String, BookList> lists = new HashMap<String, BookList>();
 
     public Book getBook(long id) {
         return books.get(id);
     }
 
-    public LazyArrayList<Book> getBooks(String url) {
+    public BookList getBooks(String url) {
         return lists.get(url);
     }
 
@@ -23,7 +23,7 @@ public class InMemoryCache implements Cache {
         books.put(book.getId(), book);
     }
 
-    public void putBooks(String url, LazyArrayList<Book> books) {
+    public void putBooks(String url, BookList books) {
         lists.put(url, books);
         for (Book book : books) {
             putBook(book);
