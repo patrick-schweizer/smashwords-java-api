c6607107974e89c25d924ccf4d14fc7abb5a5704
patrick.schweizer.ing@gmail.com <patrick.schweizer.ing@gmail.com@9488c6e7-2cef-d8bb-f510-c2d52d8d2cfe>
2012-05-25 02:17:19 +0000

    Centralized code

---
+++ SmashwordsAPI/src/main/java/com/unleashyouradventure/swapi/util/ParseUtils.java
@@ -0,0 +1,31 @@
+package com.unleashyouradventure.swapi.util;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jsoup.nodes.Element;
+
+public class ParseUtils {
+    public static abstract class Parser<T> {
+        private final static Logger log = Logger.getLogger(Parser.class.getName());
+
+        public T parse(Element element) {
+            try {
+                return parseElement(element);
+            } catch (Exception e) {
+                String elem = (element == null) ? "null" : element.toString();
+                log.log(Level.WARNING, "Parse error, Element: " + elem, e);
+            }
+            return null;
+        }
+
+        protected abstract T parseElement(Element element);
+    }
+
+    public static Integer parsePrice(String txt) {
+        txt = txt.replace(".", "");
+        txt = txt.replace(" ", "");
+        Integer price = Integer.valueOf(txt);
+        return price;
+    }
+}
