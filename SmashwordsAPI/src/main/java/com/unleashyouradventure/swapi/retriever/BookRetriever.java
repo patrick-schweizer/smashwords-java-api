3a1c934d0dd6a3174564b57434c8d5d6e63e1eb2
patrick.schweizer.ing@gmail.com <patrick.schweizer.ing@gmail.com@9488c6e7-2cef-d8bb-f510-c2d52d8d2cfe>
2012-05-25 02:10:21 +0000

    Added ProgressCallback

--- SmashwordsAPI/src/main/java/com/unleashyouradventure/swapi/retriever/BookRetriever.java
+++ SmashwordsAPI/src/main/java/com/unleashyouradventure/swapi/retriever/BookRetriever.java
@@ -11,12 +11,16 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.jsing.common.string.StringTrimmer;
 import com.unleashyouradventure.swapi.Smashwords;
 import com.unleashyouradventure.swapi.cache.Cache;
 import com.unleashyouradventure.swapi.cache.NoCache;
 import com.unleashyouradventure.swapi.load.LoginHelper;
 import com.unleashyouradventure.swapi.load.PageLoader;
+import com.unleashyouradventure.swapi.load.PageLoader.ProgressCallback;
 import com.unleashyouradventure.swapi.retriever.Book.FileType;
+import com.unleashyouradventure.swapi.util.ParseUtils;
+import com.unleashyouradventure.swapi.util.ParseUtils.Parser;
 
 public class BookRetriever {
     private final static Logger log = Logger.getLogger(BookRetriever.class.getName());
@@ -34,41 +38,108 @@ public class BookRetriever {
         this.cache = cache;
     }
 
-    public Book getBook(long id) throws IOException {
+    public Book getBook(ProgressCallback progressCallback, long id) throws IOException {
         Book book = cache.getBook(id);
         if (book == null) {
-            return getBookWithDetails(id);
+            return getBookWithDetails(progressCallback, id);
         }
         return book;
     }
 
-    public Book getBookWithDetails(long id) throws IOException {
+    public Book getBookWithDetails(ProgressCallback progressCallback, long id) throws IOException {
+        boolean loadBookInformation = false;
         Book book = cache.getBook(id);
         if (book != null && book.isBookDetailsAdded()) {
+            progressCallback.setProgress(90);
+            progressCallback.setCurrentAction("Book retrieved from cache");
             return book;
         } else if (book == null) {
+            // A book we have never heard about before -> We need to load the
+            // book information as well
+            loadBookInformation = true;
             book = new Book();
             book.setId(id);
             cache.putBook(book);
-            // A book we have never heard about before
         } else {
-            // We have to load the detail page and we write all attributes new
-            // but we still keep the same object to not break any object
-            // references.
+            // We have to load the details but we still keep the same object to
+            // not break any object references.
         }
-
+        progressCallback.setCurrentAction("Contacting Smashwords");
         String page = this.loader.getPage(Smashwords.BASE_URL + "/books/view/" + id);
         login.updateLoginStatus(page);
+        progressCallback.setProgress(50);
+        progressCallback.setCurrentAction("Parsing page");
+        Document doc = Jsoup.parse(page);
+        if (loadBookInformation) {
+            loadBookInformation(page, doc, book);
+            progressCallback.setProgress(70);
+        }
+        loadBookDetails(page, doc, book);
+        progressCallback.setProgress(90);
+        return book;
+    }
 
-        book.setBookOwned(page.contains("<a href=\"#download\">You own it!</a>"));
+    private void loadBookInformation(String rawPage, Document doc, Book book) {
+        Element elem = doc.select("div [itemtype=http://data-vocabulary.org/Product]").first();
+        book.setAuthor(authorParser.parse(elem));
+        book.setCoverUrl(coverUrlParser.parse(elem));
+        book.setDescriptionShort(descriptionParser.parse(elem));
+        book.setPriceInCent(priceParser.parse(elem));
+        book.setTitle(titleParser.parse(doc));
+    }
+
+    private void loadBookDetails(String rawPage, Document doc, Book book) throws IOException {
+        book.setBookOwned(rawPage.contains("<a href=\"#download\">You own it!</a>"));
+        loadDownloadUrls(doc, book);
+    }
+
+    private final static Parser<String> authorParser = new Parser<String>() {
+        @Override
+        protected String parseElement(Element element) {
+            Element a = element.select("a[href~=https://www.smashwords.com/profile/view/.*]").first();
+            return a.text();
+        }
+    };
+
+    private final static Parser<String> coverUrlParser = new Parser<String>() {
+        @Override
+        protected String parseElement(Element element) {
+            Element img = element.select("img[itemprop=image]").first();
+            String url = new StringTrimmer(img.attr("src")).getBeforeLast("-thumb").toString();
+            return url;
+        }
+    };
+
+    private final static Parser<String> descriptionParser = new Parser<String>() {
+        @Override
+        protected String parseElement(Element element) {
+            Element span = element.select("span[itemprop=description]").first();
+            return span.text();
+        }
+    };
+
+    private final static Parser<Integer> priceParser = new Parser<Integer>() {
+        @Override
+        protected Integer parseElement(Element element) {
+            Element span = element.select("span[itemprop=price]").first();
+            String txt = span.text();
+            return ParseUtils.parsePrice(txt);
+        }
+    };
+
+    private final static Parser<String> titleParser = new Parser<String>() {
+        @Override
+        protected String parseElement(Element element) {
+            Element h2 = element.getElementsByClass("bookpage_title_heading").first();
+            return h2.text();
+        }
+    };
+
+    private void loadDownloadUrls(Document doc, Book book) throws IOException {
 
         Element row;
         Element td;
         String url;
-        Document doc = Jsoup.parse(page);
-
-        // TODO add other attributes
-
         for (FileType type : FileType.values()) {
             Elements elements = doc.select("table tr td b:contains(" + type.getSmashwordsName() + ")");
             if (elements.size() > 0) {
@@ -83,7 +154,7 @@ public class BookRetriever {
                     // Direct download link
                     title = "Newest";
                 } else {
-                    // Leads to page with all revisions.We generate the link
+                    // Leads to page with all revisions. We generate the link
                     // ourself
                     url = url.replace("null", "newest");
                     url = url + "." + type.name().toLowerCase();
@@ -95,6 +166,9 @@ public class BookRetriever {
             }
         }
         book.setBookDetailsAdded(true);
-        return book;
+    }
+
+    public Cache getCache() {
+        return cache;
     }
 }
